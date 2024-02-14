0677044d1a3e236b18a2ccdb1c5b19e7fb7d132d
FaiqaMuhammadYousuf <faiqamuhammadyousuf@gmail.com>
2024-02-14 06:43:32 -0800

    Reserve Counting

---
+++ reverse counting/reverse counting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_counting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse Counting");
+            for (int i = 20; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
